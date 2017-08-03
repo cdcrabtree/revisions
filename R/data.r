853ed049d7fe48c79e4b96db38c7bc63ef39d01d
Charles Crabtree <crabtreedcharles@gmail.com>
2017-08-02 20:24:21 -0400

    first update

---
+++ R/data.r
@@ -0,0 +1,89 @@
+#' Macroeconomic revisions data.
+
+#'
+#' An R package that contains macroeconomic revisions data. Edit
+#'
+#' @format A data frame with 8439 rows and 79 columns:
+#' \describe{
+#'   \item{countryname}{Country Name}
+#'   \item{ccode}{Country COW Code}
+#'   \item{year}{Year}
+#'   \item{GDP1994}{GDP Data for the 1994 Revision Year}
+#'   \item{GDP1995}{GDP Data for the 1995 Revision Year}
+#'   \item{GDP1997}{GDP Data for the 1997 Revision Year}
+#'   \item{GDP1998}{GDP Data for the 1998 Revision Year}
+#'   \item{GDP1999}{GDP Data for the 1999 Revision Year}
+#'   \item{GDP2000}{GDP Data for the 2000 Revision Year}
+#'   \item{GDP2001}{GDP Data for the 2001 Revision Year}
+#'   \item{GDP2002}{GDP Data for the 2002 Revision Year}
+#'   \item{GDP2003}{GDP Data for the 2003 Revision Year}
+#'   \item{GDP2004}{GDP Data for the 2004 Revision Year}
+#'   \item{GDP2005}{GDP Data for the 2005 Revision Year}
+#'   \item{GDP2006}{GDP Data for the 2006 Revision Year}
+#'   \item{GDP2007}{GDP Data for the 2007 Revision Year}
+#'   \item{GDP2008}{GDP Data for the 2008 Revision Year}
+#'   \item{GDP2009}{GDP Data for the 2009 Revision Year}
+#'   \item{GDP2010}{GDP Data for the 2010 Revision Year}
+#'   \item{GDP2011}{GDP Data for the 2011 Revision Year}
+#'   \item{GDP2012}{GDP Data for the 2012 Revision Year}
+#'   \item{GDP2013}{GDP Data for the 2013 Revision Year}
+#'   \item{GDP2014}{GDP Data for the 2014 Revision Year}
+#'   \item{GDP2015}{GDP Data for the 2015 Revision Year}
+#'   \item{GDP2016}{GDP Data for the 2016 Revision Year}
+#'   \item{GDPPC1989}{GDP Per Capita Data for the 1989 Revision Year}
+#'   \item{GDPPC1990}{GDP Per Capita Data for the 1990 Revision Year}
+#'   \item{GDPPC1991}{GDP Per Capita Data for the 1991 Revision Year}
+#'   \item{GDPPC1992}{GDP Per Capita Data for the 1992 Revision Year}
+#'   \item{GDPPC1993}{GDP Per Capita Data for the 1993 Revision Year}
+#'   \item{GDPPC1994}{GDP Per Capita Data for the 1994 Revision Year}
+#'   \item{GDPPC1995}{GDP Per Capita Data for the 1995 Revision Year}
+#'   \item{GDPPC1997}{GDP Per Capita Data for the 1997 Revision Year}
+#'   \item{GDPPC1998}{GDP Per Capita Data for the 1998 Revision Year}
+#'   \item{GDPPC1999}{GDP Per Capita Data for the 1999 Revision Year}
+#'   \item{GDPPC2000}{GDP Per Capita Data for the 2000 Revision Year}
+#'   \item{GDPPC2001}{GDP Per Capita Data for the 2001 Revision Year}
+#'   \item{GDPPC2002}{GDP Per Capita Data for the 2002 Revision Year}
+#'   \item{GDPPC2003}{GDP Per Capita Data for the 2003 Revision Year}
+#'   \item{GDPPC2004}{GDP Per Capita Data for the 2004 Revision Year}
+#'   \item{GDPPC2005}{GDP Per Capita Data for the 2005 Revision Year}
+#'   \item{GDPPC2006}{GDP Per Capita Data for the 2006 Revision Year}
+#'   \item{GDPPC2007}{GDP Per Capita Data for the 2007 Revision Year}
+#'   \item{GDPPC2008}{GDP Per Capita Data for the 2008 Revision Year}
+#'   \item{GDPPC2009}{GDP Per Capita Data for the 2009 Revision Year}
+#'   \item{GDPPC2010}{GDP Per Capita Data for the 2010 Revision Year}
+#'   \item{GDPPC2011}{GDP Per Capita Data for the 2011 Revision Year}
+#'   \item{GDPPC2012}{GDP Per Capita Data for the 2012 Revision Year}
+#'   \item{GDPPC2013}{GDP Per Capita Data for the 2013 Revision Year}
+#'   \item{GDPPC2014}{GDP Per Capita Data for the 2014 Revision Year}
+#'   \item{GDPPC2015}{GDP Per Capita Data for the 2015 Revision Year}
+#'   \item{GDPPC2016}{GDP Per Capita Data for the 2016 Revision Year}
+#'   \item{GDPGROWTH1989}{GDP Growth Data for the 1989 Revision Year}
+#'   \item{GDPGROWTH1990}{GDP Growth Data for the 1990 Revision Year}
+#'   \item{GDPGROWTH1991}{GDP Growth Data for the 1991 Revision Year}
+#'   \item{GDPGROWTH1992}{GDP Growth Data for the 1992 Revision Year}
+#'   \item{GDPGROWTH1993}{GDP Growth Data for the 1993 Revision Year}
+#'   \item{GDPGROWTH1994}{GDP Growth Data for the 1994 Revision Year}
+#'   \item{GDPGROWTH1995}{GDP Growth Data for the 1995 Revision Year}
+#'   \item{GDPGROWTH1997}{GDP Growth Data for the 1997 Revision Year}
+#'   \item{GDPGROWTH1998}{GDP Growth Data for the 1998 Revision Year}
+#'   \item{GDPGROWTH1999}{GDP Growth Data for the 1999 Revision Year}
+#'   \item{GDPGROWTH2000}{GDP Growth Data for the 2000 Revision Year}
+#'   \item{GDPGROWTH2001}{GDP Growth Data for the 2001 Revision Year}
+#'   \item{GDPGROWTH2002}{GDP Growth Data for the 2002 Revision Year}
+#'   \item{GDPGROWTH2003}{GDP Growth Data for the 2003 Revision Year}
+#'   \item{GDPGROWTH2004}{GDP Growth Data for the 2004 Revision Year}
+#'   \item{GDPGROWTH2005}{GDP Growth Data for the 2005 Revision Year}
+#'   \item{GDPGROWTH2006}{GDP Growth Data for the 2006 Revision Year}
+#'   \item{GDPGROWTH2007}{GDP Growth Data for the 2007 Revision Year}
+#'   \item{GDPGROWTH2008}{GDP Growth Data for the 2008 Revision Year}
+#'   \item{GDPGROWTH2009}{GDP Growth Data for the 2009 Revision Year}
+#'   \item{GDPGROWTH2010}{GDP Growth Data for the 2010 Revision Year}
+#'   \item{GDPGROWTH2011}{GDP Growth Data for the 2011 Revision Year}
+#'   \item{GDPGROWTH2012}{GDP Growth Data for the 2012 Revision Year}
+#'   \item{GDPGROWTH2013}{GDP Growth Data for the 2013 Revision Year}
+#'   \item{GDPGROWTH2014}{GDP Growth Data for the 2014 Revision Year}
+#'   \item{GDPGROWTH2015}{GDP Growth Data for the 2015 Revision Year}
+#'   \item{GDPGROWTH2016}{GDP Growth Data for the 2016 Revision Year}
+#' }
+#' @source \url{http://data.worldbank.org/data-catalog/world-development-indicators}
+"gdp.wide"
